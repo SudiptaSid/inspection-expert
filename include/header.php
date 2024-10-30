a6f290677dd350474207f5d5f2f6b4d93b1170b9
Sudipta Biswas <sudipta.biswas.5648@gmail.com>
2024-10-30 13:50:45 +0530

    Initial commit

---
+++ include/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="images/favicon.png">
+
+	<!-- StyleSheet -->
+	<link rel="stylesheet" href="css/style-min.css">
+	<link rel="stylesheet" href="css/custom.css">
+
+	<title>Inspection Experts</title>
+</head>
+
+<body>
+	<!-- Loader -->
+	<div class="preloader">
+		<div class="d-table">
+			<div class="d-table-cell">
+				<div class="lds-spinner">
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Top Header -->
+	<section class="topHeader">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-md-4">
+					<a href="#" class="logoSec">
+						<img src="images/logo.png" alt="">
+					</a>
+				</div>
+				<div class="col-md-8">
+					<div class="contactInfo">
+						<a href="tel:02034042777">
+							<img src="images/icons/call.svg" alt=""> 0203 404 2777
+						</a>
+						<span></span>
+						<a href="mailto:hello@inspectionexperts.co.uk">
+							<img src="images/icons/mail.svg" alt=""> hello@inspectionexperts.co.uk
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
---
+++ index.php
@@ -0,0 +1,266 @@
+<?php include 'include/header.php'; ?>
+
+<!-- Hero Banner One -->
+<section class="heroBannerOne" style="background-image: url('images/bg/hero.jpg');">
+	<div class="container">
+		<div class="row align-items-center gap-y2">
+			<div class="col-lg-7 col-md-8">
+				<div class="leftSec">
+					<div class="sectionTitle">
+						<span></span>
+						<h1>
+							<small>Welcome to</small>
+							Inspection Experts
+						</h1>
+						<p class="paraXl">
+							Your Trusted Partner in Comprehensive Property Inventories!
+						</p>
+						<span></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 offset-lg-1">
+				<div class="rightSec">
+					<div class="bgBox">
+						<div class="sectionTitle">
+							<h5>Connect with Us For High Quality Property Inventories & Inspections</h5>
+							<p>
+								Get <b>20%</b> Discount on Your Next Order. - Use Coupon Code: <b>(IEORDER1)</b>
+							</p>
+						</div>
+						<form action="">
+							<div class="row">
+								<div class="col-12 mb-2">
+									<div class="form-group">
+										<div class="inputBox">
+											<input type="text" class="form-control" placeholder="Name">
+										</div>
+									</div>
+								</div>
+								<div class="col-12 mb-2">
+									<div class="form-group">
+										<div class="inputBox">
+											<input type="text" class="form-control" placeholder="Phone">
+										</div>
+									</div>
+								</div>
+								<div class="col-12 mb-2">
+									<div class="form-group">
+										<div class="inputBox">
+											<input type="text" class="form-control" placeholder="Email Address">
+										</div>
+									</div>
+								</div>
+								<div class="col-12 mb-2">
+									<div class="form-group">
+										<div class="inputBox">
+											<input type="text" class="form-control" placeholder="Coupon Code: IEORDER1">
+										</div>
+									</div>
+								</div>
+								<div class="col-12">
+									<button class="btn btnPrimary w-100">SUBMIT</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="taglineOne">
+					<div class="row align-items-center gap-y2">
+						<div class="col-4 text-center">
+							<div class="sectionTitle">
+								<h4>
+									<span>LOWEST</span>
+									PRICE
+								</h4>
+							</div>
+						</div>
+						<div class="col-4 text-center p-0">
+							<div class="centerSec">
+								<div class="sectionTitle">
+									<h4>
+										<span>FASTEST</span>
+										SERVICE
+									</h4>
+								</div>
+							</div>
+						</div>
+						<div class="col-4 text-center">
+							<div class="sectionTitle">
+								<h4>
+									<span>HIGHEST</span>
+									QUALITY
+								</h4>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Video One -->
+<section class="videoOne">
+	<div class="container">
+		<div class="row align-items-center gap-y2">
+			<div class="col-md-6">
+				<div class="sectionTitle">
+					<p class="paraLg">
+						Inspection Experts are professionals who are recognised for our high standard property inspection and inventory services. Click below and visit our Youtube Channel to know more!
+					</p>
+					<div class="btnSec">
+						<a href="https://youtube.com/shorts/ude7bG1_6oI?si=WIKOYzwGjA41zHA6" target="_blank" class="btn btnPrimary">
+							<img src="images/icons/youtube.svg" alt=""> Visit our youtube link
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="imgSec">
+					<img src="images/video.jpg" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Service One -->
+<section class="serviceOne">
+	<div class="container">
+		<div class="row gap-y3">
+			<div class="col-12 text-center">
+				<div class="sectionTitle">
+					<h3>
+						We can <span>help you </span>with
+					</h3>
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="row align-items-center gap-y2">
+					<div class="col-lg-3 col-md-4">
+						<div class="serviceList leftSec">
+							<div class="serviceCardOne">
+								<img src="images/icons/services/1.png" alt="">
+								<div class="content">
+									<h6>Inventory Check-in & Check-out</h6>
+									<p>
+										Clerks can now focus on inspections while we take care of the reports.
+									</p>
+								</div>
+							</div>
+							<div class="serviceCardOne">
+								<img src="images/icons/services/2.png" alt="">
+								<div class="content">
+									<h6>Gas Safety Certificate, <br> EPC and EICR</h6>
+									<p>
+										Gas Safety Electrical Safety (EICR) Energy Performance (EPC)
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-4">
+						<div class="imgSec">
+							<img src="images/1.svg" alt="">
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-4">
+						<div class="serviceList rightSec">
+							<div class="serviceCardOne">
+								<img src="images/icons/services/3.png" alt="">
+								<div class="content">
+									<h6>Mid-term Inspections</h6>
+									<p>
+										Protect your property from surprises. Book a mid-term inspection at your property today!
+									</p>
+								</div>
+							</div>
+							<div class="serviceCardOne">
+								<img src="images/icons/services/4.png" alt="">
+								<div class="content">
+									<h6>Floor Plans</h6>
+									<p>
+										Cleaning Maintenance
+										Gardening & more...
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- Report One -->
+<section class="reportOne">
+	<div class="container">
+		<div class="row gap-y3">
+			<div class="col-12 text-center">
+				<div class="sectionTitle">
+					<h3>
+						Download Our
+						<span>Sample Report</span>
+					</h3>
+					<p class="paraLg">We are all ears if you wish to get your reports amended for any reason!</p>
+				</div>
+			</div>
+			<div class="col-lg-10 col-12 offset-lg-1">
+				<div class="d-flex align-items-start navOne">
+					<div class="nav flex-md-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+						<button class="nav-link active" id="v-pills-one-tab" data-bs-toggle="pill" data-bs-target="#v-pills-one" type="button" role="tab" aria-controls="v-pills-one" aria-selected="true">Property Inventory Report</button>
+						<button class="nav-link" id="v-pills-two-tab" data-bs-toggle="pill" data-bs-target="#v-pills-two" type="button" role="tab" aria-controls="v-pills-two" aria-selected="false">Mid-Term Inspection Report</button>
+						<button class="nav-link" id="v-pills-four-tab" data-bs-toggle="pill" data-bs-target="#v-pills-four" type="button" role="tab" aria-controls="v-pills-four" aria-selected="false">Sample Floor Plan</button>
+					</div>
+					<div class="tab-content" id="v-pills-tabContent">
+						<div class="tab-pane fade show active" id="v-pills-one" role="tabpanel" aria-labelledby="v-pills-one-tab">
+							<div class="reportCardOne">
+								<div class="imgSec">
+									<img src="images/download/checkin.jpg" alt="">
+									<a href="images/download/report/checkin.pdf" class="btn btnPrimary" target="_blank" download>
+										<img src="images/icons/download.svg" alt=""> Download Sample Report
+									</a>
+								</div>
+								<p class="m-0 d-none">
+									Lorem ipsum dolor sit amet, consect matetur adipiscing elit. Nam leo lacus, dapibus a turpis et, convallis sectetur cursus turpis. Duis mattis vel erat in luctus adipiscing comus dapibus.
+								</p>
+							</div>
+						</div>
+						<div class="tab-pane fade" id="v-pills-two" role="tabpanel" aria-labelledby="v-pills-two-tab">
+							<div class="reportCardOne">
+								<div class="imgSec">
+									<img src="images/download/mid-term.jpg" alt="">
+									<a href="images/download/report/mid-term.pdf" class="btn btnPrimary" target="_blank" download>
+										<img src="images/icons/download.svg" alt=""> Download Sample Report
+									</a>
+								</div>
+								<p class="m-0 d-none">
+									Lorem ipsum dolor sit amet, consect matetur adipiscing elit. Nam leo lacus, dapibus a turpis et, convallis sectetur cursus turpis. Duis mattis vel erat in luctus adipiscing comus dapibus.
+								</p>
+							</div>
+						</div>
+						<div class="tab-pane fade" id="v-pills-four" role="tabpanel" aria-labelledby="v-pills-four-tab">
+							<div class="reportCardOne">
+								<div class="imgSec">
+									<img src="images/download/floorplan.jpg" alt="">
+									<a href="images/download/report/floorplan.pdf" class="btn btnPrimary" target="_blank" download>
+										<img src="images/icons/download.svg" alt=""> Download Sample Report
+									</a>
+								</div>
+								<p class="m-0 d-none">
+									Lorem ipsum dolor sit amet, consect matetur adipiscing elit. Nam leo lacus, dapibus a turpis et, convallis sectetur cursus turpis. Duis mattis vel erat in luctus adipiscing comus dapibus.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include 'include/footer.php'; ?>
